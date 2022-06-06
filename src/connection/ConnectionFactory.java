5cbacd7bb32cfdeb54198fbe467777f10e956fb3
erich-ika <erich.sabe.tudo@gmail.com>
2022-06-06 19:17:54 -0300

    Update MySQL JDBC Driver
    alterado o driver pra corrigir o seguinte erro:
    > "Loading class `com.mysql.jdbc.Driver'. This is deprecated. The new driver class is `com.mysql.cj.jdbc.Driver'. The driver is automatically registered via the SPI and manual loading of the driver class is generally unnecessary."

--- src/connection/ConnectionFactory.java
+++ src/connection/ConnectionFactory.java
@@ -10,7 +10,7 @@ import java.util.logging.Logger;
 
 public class ConnectionFactory {
 
-    private static final String DRIVER = "com.mysql.jdbc.Driver";
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String URL = "jdbc:mysql://database-1.csiuul0muy6o.sa-east-1.rds.amazonaws.com:3306/X_AEP_GRUPO_TOP_X";
     private static final String USER = "admin";
     private static final String PASS = "adminadmin";
