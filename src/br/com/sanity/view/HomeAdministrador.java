5d496c676026b0aa6279fedc94bc38e06f7ad3b5
erich-ika <erich.sabe.tudo@gmail.com>
2022-06-23 16:59:42 -0300

    Reimplementado tela de Formulários à home de Admin
    acontece que eu tava usando o mesmo modelo pras duas tabelas e tava quebrando tudo deu mó trabalho slk

--- src/br/com/sanity/view/HomeAdministrador.java
+++ src/br/com/sanity/view/HomeAdministrador.java
@@ -1,6 +1,7 @@
 package br.com.sanity.view;
 
 import br.com.sanity.connection.ConnectionFactory;
+import br.com.sanity.model.Formulario;
 import java.awt.CardLayout;
 import java.util.ArrayList;
 import br.com.sanity.model.Usuario;
@@ -14,12 +15,16 @@ public class HomeAdministrador extends javax.swing.JFrame {
     Usuario user;
     int usuariospag = 0;
     int usuariostotal;
-    javax.swing.table.DefaultTableModel tableModel;
+    int formspag = 0;
+    int formstotal;
+    javax.swing.table.DefaultTableModel tableModelUsuario;
+    javax.swing.table.DefaultTableModel tableModelFormulario;
 
     public HomeAdministrador(Usuario user) {
         this.user = user;
         initComponents();
         mostrarTabelaUsuarios();
+        mostrarTabelaFormularios();
         mostrarGrafico();
         nomeAdmin.setText("Olá, " + this.user.getNome());
     }
@@ -40,7 +45,7 @@ public class HomeAdministrador extends javax.swing.JFrame {
     }
 
     private void mostrarTabelaUsuarios() {
-        tableModel = (javax.swing.table.DefaultTableModel) jTable2.getModel();
+        tableModelUsuario = (javax.swing.table.DefaultTableModel) jTable2.getModel();
 
         //formata os caraio dos cabeçalho da tabela
         final javax.swing.table.DefaultTableCellRenderer renderer = new javax.swing.table.DefaultTableCellRenderer();
@@ -78,7 +83,7 @@ public class HomeAdministrador extends javax.swing.JFrame {
             public void mouseClicked(java.awt.event.MouseEvent e) {
                 javax.swing.JTable selected = (javax.swing.JTable) e.getSource();
                 if (selected.getSelectedColumn() == 3) {
-                    new AlterarUsuario(user, (int) tableModel.getValueAt(selected.getSelectedRow(), 0)).setVisible(true);
+                    new AlterarUsuario(user, (int) tableModelUsuario.getValueAt(selected.getSelectedRow(), 0)).setVisible(true);
                 }
             }
         });
@@ -88,7 +93,7 @@ public class HomeAdministrador extends javax.swing.JFrame {
     }
 
     private void mostrarTabelaFormularios() {
-        tableModel = (javax.swing.table.DefaultTableModel) jtListaFormulario.getModel();
+        tableModelFormulario = (javax.swing.table.DefaultTableModel) jtListaFormulario.getModel();
 
         //formata os caraio dos cabeçalho da tabela
         final javax.swing.table.DefaultTableCellRenderer renderer = new javax.swing.table.DefaultTableCellRenderer();
@@ -124,7 +129,7 @@ public class HomeAdministrador extends javax.swing.JFrame {
             public void mouseClicked(java.awt.event.MouseEvent e) {
                 javax.swing.JTable selected = (javax.swing.JTable) e.getSource();
                 if (selected.getSelectedColumn() == 2) {
-                    new CadastroFormulario(user, (int) tableModel.getValueAt(selected.getSelectedRow(), 0)).setVisible(true);
+                    new CadastroFormulario(user, (int) tableModelFormulario.getValueAt(selected.getSelectedRow(), 0)).setVisible(true);
                 }
             }
         });
@@ -164,7 +169,16 @@ public class HomeAdministrador extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
         formularios = new javax.swing.JPanel();
-        jButton1 = new javax.swing.JButton();
+        mainzinho2 = new javax.swing.JPanel();
+        nav2 = new javax.swing.JPanel();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        jtListaFormulario = new javax.swing.JTable();
+        btnAvancar1 = new javax.swing.JButton();
+        btnVoltar1 = new javax.swing.JButton();
+        btnPesquisar1 = new javax.swing.JButton();
+        txtPesquisar = new javax.swing.JTextField();
+        jpTitulo = new javax.swing.JPanel();
+        lblTitulo = new javax.swing.JLabel();
 
         botao.setText("jButton3");
 
@@ -468,29 +482,156 @@ public class HomeAdministrador extends javax.swing.JFrame {
 
         main.add(estatisticas, "estatisticas");
 
-        jButton1.setText("adicionar");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        formularios.setBackground(new java.awt.Color(102, 102, 102));
+        formularios.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        mainzinho2.setBackground(new java.awt.Color(51, 51, 51));
+        mainzinho2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        nav2.setBackground(new java.awt.Color(51, 51, 51));
+
+        jScrollPane3.setBorder(null);
+
+        jtListaFormulario.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null}
+            },
+            new String [] {
+                "ID", "Título", ""
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.Integer.class, java.lang.String.class, java.lang.Object.class
+            };
+            boolean[] canEdit = new boolean [] {
+                true, false, false
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jtListaFormulario.setAutoResizeMode(javax.swing.JTable.AUTO_RESIZE_NEXT_COLUMN);
+        jtListaFormulario.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        jtListaFormulario.setGridColor(new java.awt.Color(255, 255, 255));
+        jtListaFormulario.setRowSelectionAllowed(false);
+        jtListaFormulario.getTableHeader().setResizingAllowed(false);
+        jtListaFormulario.getTableHeader().setReorderingAllowed(false);
+        jScrollPane3.setViewportView(jtListaFormulario);
+
+        btnAvancar1.setBackground(new java.awt.Color(51, 51, 51));
+        btnAvancar1.setForeground(new java.awt.Color(251, 251, 251));
+        btnAvancar1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/com/sanity/icons/setinha.png"))); // NOI18N
+        btnAvancar1.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        btnAvancar1.setContentAreaFilled(false);
+        btnAvancar1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnAvancar1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                btnAvancar1ActionPerformed(evt);
             }
         });
 
-        javax.swing.GroupLayout formulariosLayout = new javax.swing.GroupLayout(formularios);
-        formularios.setLayout(formulariosLayout);
-        formulariosLayout.setHorizontalGroup(
-            formulariosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, formulariosLayout.createSequentialGroup()
-                .addContainerGap(450, Short.MAX_VALUE)
-                .addComponent(jButton1)
-                .addGap(330, 330, 330))
+        btnVoltar1.setBackground(new java.awt.Color(51, 51, 51));
+        btnVoltar1.setForeground(new java.awt.Color(251, 251, 251));
+        btnVoltar1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/com/sanity/icons/setinha_esquerda.png"))); // NOI18N
+        btnVoltar1.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        btnVoltar1.setContentAreaFilled(false);
+        btnVoltar1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnVoltar1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnVoltar1ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout nav2Layout = new javax.swing.GroupLayout(nav2);
+        nav2.setLayout(nav2Layout);
+        nav2Layout.setHorizontalGroup(
+            nav2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(nav2Layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addGroup(nav2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(nav2Layout.createSequentialGroup()
+                        .addComponent(btnVoltar1, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(591, 591, 591)
+                        .addComponent(btnAvancar1))
+                    .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 641, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(0, 0, Short.MAX_VALUE))
         );
-        formulariosLayout.setVerticalGroup(
-            formulariosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(formulariosLayout.createSequentialGroup()
-                .addGap(185, 185, 185)
-                .addComponent(jButton1)
-                .addContainerGap(333, Short.MAX_VALUE))
+        nav2Layout.setVerticalGroup(
+            nav2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(nav2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane3, javax.swing.GroupLayout.DEFAULT_SIZE, 283, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(nav2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(btnAvancar1)
+                    .addComponent(btnVoltar1))
+                .addContainerGap())
         );
+
+        mainzinho2.add(nav2, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 40, -1, -1));
+
+        btnPesquisar1.setBackground(new java.awt.Color(51, 51, 51));
+        btnPesquisar1.setForeground(new java.awt.Color(251, 251, 251));
+        btnPesquisar1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/com/sanity/icons/lupa17.png"))); // NOI18N
+        btnPesquisar1.setText(" Pesquisar");
+        btnPesquisar1.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        btnPesquisar1.setContentAreaFilled(false);
+        btnPesquisar1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnPesquisar1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnPesquisar1ActionPerformed(evt);
+            }
+        });
+        mainzinho2.add(btnPesquisar1, new org.netbeans.lib.awtextra.AbsoluteConstraints(580, 20, 101, 19));
+
+        txtPesquisar.setBackground(new java.awt.Color(102, 102, 102));
+        txtPesquisar.setForeground(new java.awt.Color(255, 255, 255));
+        txtPesquisar.setToolTipText("");
+        txtPesquisar.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        txtPesquisar.setCaretColor(new java.awt.Color(51, 51, 51));
+        mainzinho2.add(txtPesquisar, new org.netbeans.lib.awtextra.AbsoluteConstraints(370, 20, 210, -1));
+
+        formularios.add(mainzinho2, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 90, 710, 390));
+
+        jpTitulo.setBackground(new java.awt.Color(255, 0, 66));
+
+        lblTitulo.setBackground(new java.awt.Color(255, 0, 102));
+        lblTitulo.setFont(new java.awt.Font("Tahoma", 1, 19)); // NOI18N
+        lblTitulo.setForeground(new java.awt.Color(251, 251, 251));
+        lblTitulo.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblTitulo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/com/sanity/icons/search_26px.png"))); // NOI18N
+        lblTitulo.setText("FORMULÁRIOS");
+        lblTitulo.setToolTipText("");
+
+        javax.swing.GroupLayout jpTituloLayout = new javax.swing.GroupLayout(jpTitulo);
+        jpTitulo.setLayout(jpTituloLayout);
+        jpTituloLayout.setHorizontalGroup(
+            jpTituloLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jpTituloLayout.createSequentialGroup()
+                .addGap(176, 176, 176)
+                .addComponent(lblTitulo, javax.swing.GroupLayout.PREFERRED_SIZE, 355, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(179, Short.MAX_VALUE))
+        );
+        jpTituloLayout.setVerticalGroup(
+            jpTituloLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(lblTitulo, javax.swing.GroupLayout.DEFAULT_SIZE, 40, Short.MAX_VALUE)
+        );
+
+        formularios.add(jpTitulo, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 50, 710, -1));
 
         main.add(formularios, "formularios");
 
@@ -525,7 +666,7 @@ public class HomeAdministrador extends javax.swing.JFrame {
 
     private void btnPesquisarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnPesquisarActionPerformed
         usuariospag = 0;
-        usuariostotal = preencherTabela(this.user, usuariospag, checkColaborador.isSelected(), checkAdministrador.isSelected());
+        usuariostotal = preencherTabelaUsuarios(this.user, usuariospag, checkColaborador.isSelected(), checkAdministrador.isSelected());
     }//GEN-LAST:event_btnPesquisarActionPerformed
 
     private void checkAdministradorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_checkAdministradorActionPerformed
@@ -549,7 +690,7 @@ public class HomeAdministrador extends javax.swing.JFrame {
             return; //fazer nada se na primeira página
         }
         usuariospag--;
-        usuariostotal = preencherTabela(user, usuariospag, checkColaborador.isSelected(), checkAdministrador.isSelected());
+        usuariostotal = preencherTabelaUsuarios(user, usuariospag, checkColaborador.isSelected(), checkAdministrador.isSelected());
     }//GEN-LAST:event_btnVoltarActionPerformed
 
     private void btnAvancarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAvancarActionPerformed
@@ -557,24 +698,40 @@ public class HomeAdministrador extends javax.swing.JFrame {
             return; //fazer nada se na última página
         }
         usuariospag++;
-        usuariostotal = preencherTabela(user, usuariospag, checkColaborador.isSelected(), checkAdministrador.isSelected());
+        usuariostotal = preencherTabelaUsuarios(user, usuariospag, checkColaborador.isSelected(), checkAdministrador.isSelected());
     }//GEN-LAST:event_btnAvancarActionPerformed
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        new CadastroFormulario(user).setVisible(true);
-    }//GEN-LAST:event_jButton1ActionPerformed
+    private void btnAvancar1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAvancar1ActionPerformed
+        if (formspag >= formstotal / 10) {
+            return; //fazer nada se na última página
+        }
+        formspag++;
+        formstotal = preencherTabelaFormularios(user, formspag);
+    }//GEN-LAST:event_btnAvancar1ActionPerformed
 
-    private int preencherTabela(Usuario user, int pagina, boolean c, boolean a) {
+    private void btnVoltar1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVoltar1ActionPerformed
+        if (formspag <= 0) {
+            return; //fazer nada se na primeira página
+        }
+        formspag--;
+        formstotal = preencherTabelaFormularios(user, formspag);
+    }//GEN-LAST:event_btnVoltar1ActionPerformed
+
+    private void btnPesquisar1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnPesquisar1ActionPerformed
+        //preecherTabela(this.user, 0, checkColaborador.isSelected(), checkAdministrador.isSelected());
+    }//GEN-LAST:event_btnPesquisar1ActionPerformed
+
+    private int preencherTabelaUsuarios(Usuario user, int pagina, boolean c, boolean a) {
         //limpa tabela
-        int rowCount = tableModel.getRowCount();
+        int rowCount = tableModelUsuario.getRowCount();
         for (int i = 0; i < rowCount; i++) {
-            tableModel.removeRow(0);
+            tableModelUsuario.removeRow(0);
         }
 
         //preenche a tabela
         ArrayList<Usuario> busca = ConnectionFactory.getDezColaboradores(user, pagina, c, a);
         for (int i = 0; i < busca.size(); i++) {
-            tableModel.addRow(
+            tableModelUsuario.addRow(
                     new Object[]{
                         busca.get(i).getId(),
                         busca.get(i).getNome(),
@@ -588,12 +745,38 @@ public class HomeAdministrador extends javax.swing.JFrame {
         return ConnectionFactory.getTotalColaboradores(user, c, a);
     }
 
+    private int preencherTabelaFormularios(Usuario user, int pagina) {
+        //limpa tabela
+        int rowCount = tableModelFormulario.getRowCount();
+        for (int i = 0; i < rowCount; i++) {
+            tableModelFormulario.removeRow(0);
+        }
+
+        //preenche a tabela
+        ArrayList<Formulario> busca = ConnectionFactory.getDezFormularios(user, pagina);
+        for (int i = 0; i < busca.size(); i++) {
+            tableModelFormulario.addRow(
+                    new Object[]{
+                        busca.get(i).getId(),
+                        busca.get(i).getTitulo(),
+                        "Editar"
+                    }
+            );
+        }
+
+        //busca e retorna a quantidade
+        return ConnectionFactory.getTotalFormularios(user);
+    }
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton addUsuario;
     private javax.swing.JButton botao;
     private javax.swing.JButton btnAvancar;
+    private javax.swing.JButton btnAvancar1;
     private javax.swing.JButton btnPesquisar;
+    private javax.swing.JButton btnPesquisar1;
     private javax.swing.JButton btnVoltar;
+    private javax.swing.JButton btnVoltar1;
     private javax.swing.JButton buttonEstatistica;
     private javax.swing.JButton buttonForms;
     private javax.swing.JButton buttonHome;
@@ -602,21 +785,27 @@ public class HomeAdministrador extends javax.swing.JFrame {
     private javax.swing.JPanel dashboard1;
     private javax.swing.JPanel estatisticas;
     private javax.swing.JPanel formularios;
-    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
     private javax.swing.JTable jTable1;
     private javax.swing.JTable jTable2;
+    private javax.swing.JPanel jpTitulo;
+    private javax.swing.JTable jtListaFormulario;
+    private javax.swing.JLabel lblTitulo;
     private javax.swing.JLabel logoSanity;
     private javax.swing.JPanel main;
     private javax.swing.JPanel mainzinho;
+    private javax.swing.JPanel mainzinho2;
     private javax.swing.JPanel nav;
+    private javax.swing.JPanel nav2;
     private javax.swing.JLabel nomeAdmin;
     private javax.swing.JLabel nomeSanity;
     private javax.swing.JPanel panelCinza;
     private javax.swing.JPanel panelRosa;
+    private javax.swing.JTextField txtPesquisar;
     private javax.swing.JPanel usuarios;
     // End of variables declaration//GEN-END:variables
 }
